df4b2978c30d16a27f6feb486b65ba554aa7862a
hungrymonkey <psuedofinnish@psuedos-MacBook-Pro.local>
2018-11-16 17:53:45 -0800

    fix the drawer

--- lib/map_home.dart
+++ lib/map_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'map_types.dart';
-import 'map_drawer.dart';
+import 'map_settings.dart';
 class MapHome extends StatefulWidget {
   const MapHome( this.configuration, this.updater);
 
@@ -13,6 +13,21 @@ class MapHome extends StatefulWidget {
 
 class MapHomeState extends State<MapHome> {
 
+
+  void _handleCarChange(bool value) {
+    sendUpdates(widget.configuration.copywith(car: value));
+  }
+  void _handleTransitChange(bool value) {
+    sendUpdates(widget.configuration.copywith(transit: value));
+  }
+  void _handleScooterChange(bool value) {
+    sendUpdates(widget.configuration.copywith(scooter: value));
+  }
+
+  void _handleWalkingChange(bool value) {
+    sendUpdates(widget.configuration.copywith(walking: value));
+  }
+
   void configurationUpdater(MapConfiguration value){
     sendUpdates(value);
   }
@@ -22,7 +37,58 @@ class MapHomeState extends State<MapHome> {
   }
 
   Widget _buildDrawer(BuildContext context) {
-    return MapDrawer(widget.configuration, configurationUpdater);
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          const DrawerHeader(child: Center(child: Text('Transportation Options'))),
+          ListTile(
+              leading: const Icon(Icons.directions_car),
+              title: const Text('Select Car'),
+              onTap: () { _handleCarChange(!widget.configuration.car); },
+              trailing: Switch(
+                value: widget.configuration.car,
+                onChanged: _handleCarChange,
+              ),
+          ),
+          ListTile(
+              leading: const Icon(Icons.directions_bike),
+              title: const Text('Select Scooter'),
+              onTap: () { _handleScooterChange(!widget.configuration.scooter); },
+              trailing: Switch(
+                value: widget.configuration.scooter,
+                onChanged: _handleScooterChange,
+              ),
+          ),
+          ListTile(
+              leading: const Icon(Icons.directions_transit),
+              title: const Text('Select Transit'),
+              onTap: () { _handleTransitChange(!widget.configuration.transit); },
+              trailing: Switch(
+                value: widget.configuration.transit,
+                onChanged: _handleTransitChange,
+              ),
+          ),
+          ListTile(
+              leading: const Icon(Icons.directions_walk),
+              title: const Text('Select Walking'),
+              onTap: () { _handleWalkingChange(!widget.configuration.walking); },
+              trailing: Switch(
+                value: widget.configuration.walking,
+                onChanged: _handleWalkingChange,
+              ),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: _handleShowSettings,
+          ),
+        ],
+      ),
+    );
+  }
+  void _handleShowSettings() {
+    Navigator.popAndPushNamed(context, '/settings');
   }
   @override
     Widget build(BuildContext context) {
--- lib/map_settings.dart
+++ lib/map_settings.dart
@@ -21,59 +21,33 @@ class MapSettingsState extends State<MapSettings> {
     sendUpdates(widget.configuration.copywith(scooter: value));
   }
 
-
   void _handleWalkingChange(bool value) {
     sendUpdates(widget.configuration.copywith(walking: value));
   }
 
-
+  Widget buildAppBar(BuildContext context) {
+      return AppBar(
+        title: const Text('Settings')
+      );
+  }
   void sendUpdates(MapConfiguration value) {
     if (widget.updater != null)
       widget.updater(value);
   }
   
+
+
   Widget buildSettingsPane(BuildContext context) {
     final List<Widget> rows = <Widget>[
-      ListTile(
-          leading: const Icon(Icons.directions_car),
-          title: const Text('Select Car'),
-          onTap: () { _handleCarChange(!widget.configuration.car); },
-          trailing: Switch(
-            value: widget.configuration.car,
-            onChanged: _handleCarChange,
-          ),
-      ),
-      ListTile(
-          leading: const Icon(Icons.directions_bike),
-          title: const Text('Select Scooter'),
-          onTap: () { _handleScooterChange(!widget.configuration.scooter); },
-          trailing: Switch(
-            value: widget.configuration.scooter,
-            onChanged: _handleScooterChange,
-          ),
-      ),
-      ListTile(
-          leading: const Icon(Icons.directions_transit),
-          title: const Text('Select Transit'),
-          onTap: () { _handleTransitChange(!widget.configuration.transit); },
-          trailing: Switch(
-            value: widget.configuration.transit,
-            onChanged: _handleTransitChange,
-          ),
-      ),
-      ListTile(
-          leading: const Icon(Icons.directions_walk),
-          title: const Text('Select Walking'),
-          onTap: () { _handleWalkingChange(!widget.configuration.walking); },
-          trailing: Switch(
-            value: widget.configuration.walking,
-            onChanged: _handleWalkingChange,
-          ),
-      ),
+      
     ];
+    return ListView(children: rows);
   }
   @override
   Widget build(BuildContext context){
-    return buildSettingsPane(context);
+    return Scaffold(
+      appBar: AppBar(title: Text("Application Settings"),),
+      body: Center(child: Text("Settings")),
+    );
   }
 }
